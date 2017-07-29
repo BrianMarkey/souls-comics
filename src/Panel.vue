b5435d09bdd49f15994d0faa684be0c73e2f6e02
markey <bmarkey@wyzant.com>
2017-07-29 16:29:59 -0400

    Add the panel component.

---
+++ src/Panel.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <video v-if="panel"
+        playsinline="true"
+        webkit-playsinline=""
+        poster=""
+        class="panel-video"
+        ref="vids">
+        <source id="webmSource" v-bind:src="panel.source" type="video/webm">
+  </video>
+</template>
+
+
+<script>
+  export default {
+    name: 'panel',
+    props: ['panel'],
+    data: function () {
+      return {
+      }
+    },
+    beforeMount: function () {
+      console.log(this.panel);
+    }
+  }
+</script>
