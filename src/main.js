c500f289a5998e3b9c9ff6a088aeab9a8931e74d
markey <bmarkey@wyzant.com>
2017-08-01 13:24:49 -0400

    Refactor routes.'

--- src/main.js
+++ src/main.js
@@ -8,42 +8,36 @@ import panelsService from 'src/services/panels-service.js'
 require.context("static/", true);
 
 const VueTouch = require('vue-touch');
-const services = { panelsService };
 
 Vue.use(VueRouter);
 Vue.use(VueTouch, { name: 'v-touch' });
 Vue.component('panel', Panel);
 
+function getPropsData(stripUrlName, panelNumber) {
+
+  return {
+    strips: data.strips,
+    stripUrlName: stripUrlName || data.strips[0].urlName,
+    panelNumber: panelNumber || 1,
+    panelsService: panelsService
+  };
+}
+
 const routes = [
   {
     path: '/:stripUrlName',
     component: Comic,
-    props: (route) => ({
-                        strips: data.strips,
-                        stripUrlName: route.params.stripUrlName,
-                        panelNumber: 1,
-                        panelsService: panelsService
-                      })
+    props: (route) => (getPropsData(route.params.stripUrlName))
   },
   {
     path: '/:stripUrlName/panels/:panelNumber',
     component: Comic,
-    props: (route) => ({
-                        strips: data.strips,
-                        stripUrlName: route.params.stripUrlName,
-                        panelNumber: route.params.panelNumber,
-                        panelsService: panelsService
-                      })
+    props: (route) => (getPropsData(route.params.stripUrlName, route.params.panelNumber))
   },
   {
     path: '/',
     component: Comic,
-    props: (route) => ({
-                        strips: data.strips,
-                        stripUrlName: data.strips[0].urlName,
-                        panelNumber: 1,
-                        panelsService: panelsService
-                      })
+    props: (route) => (getPropsData())
   }
 ]
 
